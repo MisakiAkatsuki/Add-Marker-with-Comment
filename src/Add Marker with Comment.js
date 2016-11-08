d5f870c93918ffa09686b7d10cf25558bd3bc340
Misaki_Akatsuki <mikuchixip@live.jp>
2016-11-09 04:35:32 +0900

    Packeging 110 zip

--- src/Add Marker with Comment.js
+++ src/Add Marker with Comment.js
@@ -1,19 +1,3 @@
-/*
-  Add Marker with Comment
-    (C) あかつきみさき(みくちぃP)
-
-  このスクリプトについて
-    選択したレイヤーにプロンプト入力したテキストのコメント付きマーカーを追加するスクリプト.
-
-  使用方法
-    1.ファイル→スクリプト→スクリプトファイルの実行から実行.
-
-  動作環境
-    Adobe After Effects CS6以上
-
-  バージョン情報
-    2016/10/23 Ver 1.0.0 Release
-*/
 /// <reference path="C:/Users/RUI/OneDrive/lib/aftereffects.d.ts/ae.d.ts"/>
 (function () {
     var ADBE_MARKER = "ADBE Marker";
@@ -33,24 +17,33 @@
             return true;
         }
     };
-    var actComp = app.project.activeItem;
-    if (!isCompActive(actComp)) {
+    var addMarker = function () {
+        var actComp = app.project.activeItem;
+        if (!isCompActive(actComp)) {
+            return 0;
+        }
+        var selLayers = actComp.selectedLayers;
+        if (!isLayerSelected(selLayers)) {
+            var compMarkerText = prompt("Comment\nComp", "", "Add Marker with Comment");
+            if (compMarkerText == null) {
+                return 0;
+            }
+            actComp.markerProperty.setValueAtTime(actComp.time, new MarkerValue(compMarkerText));
+            return 0;
+        }
+        var layerMarkerText = prompt("Comment\nLayers", "", "Add Marker with Comment");
+        if (layerMarkerText == null) {
+            return 0;
+        }
+        var curPro;
+        for (var _i = 0, selLayers_1 = selLayers; _i < selLayers_1.length; _i++) {
+            var selLayer = selLayers_1[_i];
+            curPro = selLayer.property(ADBE_MARKER);
+            curPro.setValueAtTime(actComp.time, new MarkerValue(layerMarkerText));
+        }
         return 0;
-    }
-    var selLayers = actComp.selectedLayers;
-    if (!isLayerSelected(selLayers)) {
-        return 0;
-    }
+    };
     app.beginUndoGroup("Add Marker with Comment");
-    var text = prompt("Comment", "", "Add Marker with Comment");
-    if (text == null) {
-        return 0;
-    }
-    var markerObj = new MarkerValue(text);
-    var curPro;
-    for (var i = 0; i < selLayers.length; i++) {
-        curPro = selLayers[i].property(ADBE_MARKER);
-        curPro.setValueAtTime(actComp.time, markerObj);
-    }
+    addMarker();
     app.endUndoGroup();
 }).call(this);
